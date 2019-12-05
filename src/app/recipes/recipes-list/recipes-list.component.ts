06bb79806280527aa3e3941f1729e0e3c814b917
pravekumar1 <pravekumar1@gmail.com>
2019-12-05 16:50:24 +0530

    4 files modified

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Recipes} from '../recipes.model';
+
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-
+  recipesList: Recipes[] = [
+    new Recipes('A test Recipe', 'This is a description part', 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/caponata-pasta_1.jpg'),
+     new Recipes('A test Recipe', 'This is a description part', 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/caponata-pasta_1.jpg'),
+  ]
   constructor() { }
 
   ngOnInit() {
