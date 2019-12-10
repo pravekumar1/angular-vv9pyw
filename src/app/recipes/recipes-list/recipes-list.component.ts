8bc35659f592d0b4569c416668a2c5d8da28d446
pravekumar1 <pravekumar1@gmail.com>
2019-12-10 16:43:24 +0530

    2 files modified

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -17,9 +17,9 @@ export class RecipesListComponent implements OnInit {
       "https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/caponata-pasta_1.jpg"
     ),
     new Recipes(
-      "A test Recipe",
-      "This is a description part",
-      "https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/caponata-pasta_1.jpg"
+      "A second Recipe",
+      "This is a second description part",
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/1472483520-springtacos-1572537127.jpg?crop=1.00xw:0.672xh;0,0.156xh&resize=640:*"
     )
   ];
   constructor() {}
