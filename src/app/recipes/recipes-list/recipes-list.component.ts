5f8f83f586e1bd75e1f1741ada0a21349dc6e6d7
pravekumar1 <pravekumar1@gmail.com>
2019-12-10 15:01:01 +0530

    1 file modified

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,17 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-import {Recipes} from '../recipes.model';
-import {RecipeServiceService} from '../recipe-service.service';
+import { Recipes } from "../recipes.model";
+import { RecipeServiceService } from "../recipe-service.service";
 
 @Component({
-  selector: 'app-recipes-list',
-  templateUrl: './recipes-list.component.html',
-  styleUrls: ['./recipes-list.component.css']
+  selector: "app-recipes-list",
+  templateUrl: "./recipes-list.component.html",
+  styleUrls: ["./recipes-list.component.css"]
 })
 export class RecipesListComponent implements OnInit {
-  constructor() { }
+  recipesList: Recipes[] = [
+    new Recipes(
+      "A test Recipe",
+      "This is a description part",
+      "https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/caponata-pasta_1.jpg"
+    ),
+    new Recipes(
+      "A test Recipe",
+      "This is a description part",
+      "https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/caponata-pasta_1.jpg"
+    )
+  ];
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
