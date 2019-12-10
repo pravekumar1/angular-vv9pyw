0e13577fc9bd6e49de006f84f71c32114cd192ed
pravekumar1 <pravekumar1@gmail.com>
2019-12-10 15:06:03 +0530

    3 files modified

--- src/app/recipes/recipes-list/recipes-item/recipes-item.component.ts
+++ src/app/recipes/recipes-list/recipes-item/recipes-item.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Recipes } from "../../recipes.model";
 
 @Component({
   selector: 'app-recipes-item',
@@ -6,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-item.component.css']
 })
 export class RecipesItemComponent implements OnInit {
-
+@Input() recipeItem: Recipes
   constructor() { }
 
   ngOnInit() {
