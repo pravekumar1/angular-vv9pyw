1f33b7aa539544f9e405b1abc8355ef1af57c6a5
pravekumar1 <pravekumar1@gmail.com>
2019-12-19 19:43:27 +0530

    2 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,28 @@
-import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
 import { RecipesComponent } from "./recipes/recipes.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
+import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
+import { RecipesDetailsComponent } from "./recipes/recipes-details/recipes-details.component";
+
 
 const appRoutes: Routes = [
-  {path: '', redirectTo: '/recipes', pathMatch: 'full'},
-  {path: 'recipes', component: RecipesComponent, children: [
-    {path: }]},
-  {path: 'shopping-list', component: ShoppingListComponent}
+  { path: "", redirectTo: "/recipes", pathMatch: "full" },
+  {
+    path: "recipes",
+    component: RecipesComponent,
+    children: [
+      { path: "", component: RecipeStartComponent },
+      {path: ':id', component: RecipesDetailsComponent},
+      // {path: ':id', component: RecipesDetailsComponent},
+      ]
+  },
+  { path: "shopping-list", component: ShoppingListComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
-
-export class AppRoutingModule {
-
-}
+export class AppRoutingModule {}
