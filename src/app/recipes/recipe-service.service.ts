2c37704711c459c834a994f83f4fad6e5765008d
pravekumar1 <pravekumar1@gmail.com>
2019-12-26 17:43:54 +0530

    1 file modified

--- src/app/recipes/recipe-service.service.ts
+++ src/app/recipes/recipe-service.service.ts
@@ -9,26 +9,26 @@ import { ShoppingListService } from "../shopping-list/shopping-list.service";
 export class RecipeServiceService {
   recipesChanged = new Subject<Recipes[]>();
   private recipesList: Recipes[] = [
-    // new Recipes(
-    //   "A test Recipe",
-    //   "This is a description part",
-    //   "https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/caponata-pasta_1.jpg",
-    //   [
-    //     new Ingradients("Apple", 100),
-    //     new Ingradients("Oranges", 10),
-    //     new Ingradients("Banana", 60)
-    //   ]
-    // ),
-    // new Recipes(
-    //   "A Second Recipe value",
-    //   "This is a second description part",
-    //   "https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/epic-summer-salad.jpg",
-    //   [
-    //     new Ingradients("Apple", 15),
-    //     new Ingradients("Oranges", 20),
-    //     new Ingradients("Banana", 10)
-    //   ]
-    // )
+    new Recipes(
+      "A test Recipe",
+      "This is a description part",
+      "https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/caponata-pasta_1.jpg",
+      [
+        new Ingradients("Apple", 100),
+        new Ingradients("Oranges", 10),
+        new Ingradients("Banana", 60)
+      ]
+    ),
+    new Recipes(
+      "A Second Recipe value",
+      "This is a second description part",
+      "https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/epic-summer-salad.jpg",
+      [
+        new Ingradients("Apple", 15),
+        new Ingradients("Oranges", 20),
+        new Ingradients("Banana", 10)
+      ]
+    )
   ];
 
   // receipeSelected = new Subject<Recipes>();
